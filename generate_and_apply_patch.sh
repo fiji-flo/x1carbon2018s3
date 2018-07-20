c5e15cad0b3ee92eddc033d431098490ec49c3e0
Tommy Nevtelen <tommy.nevtelen@kambi.com>
2018-07-20 23:05:05 +0200

    Further enhancements
    * Bail on errors
    * install tools automatically on systems with apt
    * some descriptions of what's happening

--- generate_and_apply_patch.sh
+++ generate_and_apply_patch.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
+set -e
+set -o pipefail
+
 BOOT=${1:-/boot}
 IRFS_HOOK="/etc/initramfs-tools/hooks/acpi_override.sh"
 cd `mktemp -d`
 
+# Make sure we have required tools on systems with apt
+if [ -x $(which apt-get) ]; then
+	echo "[*] Installing required tools"
+	sudo apt-get -y install acpica-tools cpio
+fi
+
 # extract dsdt
+echo "[*] Dumping DSDT"
 sudo cat /sys/firmware/acpi/tables/DSDT > dsdt.dat
+
 # decompile
+echo "[*] Decompiling DSDT"
 iasl -d dsdt.dat
-
 cp dsdt.dsl dsdt.dsl.orig
-
+echo "[*] Patching DSDT"
 cat dsdt.dsl |
   tr '\n' '\r' |
   sed -e 's|DefinitionBlock ("", "DSDT", 2, "LENOVO", "SKL     ", 0x00000000)|DefinitionBlock ("", "DSDT", 2, "LENOVO", "SKL     ", 0x00000001)|' \
@@ -21,8 +32,11 @@ cat dsdt.dsl |
 mv dsdt_patched.dsl dsdt.dsl
 
 # compile
+echo "[*] Compiling DSDT"
 iasl -tc -ve dsdt.dsl
+
 # generate override
+echo "[*] Generating acpi_override"
 mkdir -p kernel/firmware/acpi
 cp dsdt.aml kernel/firmware/acpi
 find kernel | cpio -H newc --create > acpi_override
@@ -33,6 +47,7 @@ sudo cp acpi_override ${BOOT} || \
 
 # check if we have initramfs and prepend our stuff
 if [ -d $(dirname "${IRFS_HOOK}") ] && [ -x $(which update-initramfs) ]; then
+	echo "[*] Adding hook to initramfs"
 	sudo bash -c "cat > ${IRFS_HOOK} <<- HOOK
 	#!/bin/sh
 	. /usr/share/initramfs-tools/hook-functions
@@ -40,6 +55,7 @@ if [ -d $(dirname "${IRFS_HOOK}") ] && [ -x $(which update-initramfs) ]; then
 	HOOK"
 	sudo chmod +x ${IRFS_HOOK}
 	sudo update-initramfs -u -k all
+	echo "[*] Done!"
 else
-    echo "Don't forget to update your bootloader config."
+    echo "Done! Don't forget to update your bootloader config."
 fi
